36d99f0c4a1ebabd9091bbf520dcdec2dcbdec6a
marcotvaleriot <64273582+marcotvaleriot@users.noreply.github.com>
2020-04-28 02:00:12 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+#include "personatec.h"
+#include "estudiantes.h
+#include "profesores.h"
+#include "mate.h"
+
+int main()
+{
+    Estudiantes e1(18, "Marco", "A01706518", "ICI", 2);
+    Profesores p1(30, "Juan Soto", "Catedra", "Negocios", 30000);
+
+
+    cout << e1.getAge() << ", " << e1.getOfi() << ", " << e1.getNom() << ", " << e1.getMat() << ", " << e1.getAve() << ", " << e1.getSem() << endl;
+
+
+
+
+    return 0;
+}
