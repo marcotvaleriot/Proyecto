452eaa2dd45c3c4c77d156189ad36acd63468739
marcotvaleriot <64273582+marcotvaleriot@users.noreply.github.com>
2020-04-29 23:29:36 -0500

    Update Estudiantes.h

--- Estudiantes.h
+++ Estudiantes.h
@@ -8,14 +8,14 @@
 #include "PersonaTec.h"
 #include "Residencia.h"
 
-class Estudiantes: public PersonaTec{
+class Estudiantes: public PersonaTec{ //Creamos la clase de estudiantes y le hacemos herencia a personastec
     private://Atributos
         string mat;
         string ave;
         int sem;
     public://Metodos
-        Estudiantes (int edad, string nombre, string matricula, string avenida, int semestre, Residencia hogar):
-        PersonaTec(edad, "Estudiante", nombre, hogar){ //Constructor
+        Estudiantes (int edad, string nombre, string matricula, string avenida, int semestre, Residencia hogar): //El constructor de estudiantes
+        PersonaTec(edad, "Estudiante", nombre, hogar){ //Los objetos que hereda de personatec, y el default que es el oficio 
             mat = matricula;
             ave = avenida;
             sem = semestre;
@@ -38,9 +38,9 @@ class Estudiantes: public PersonaTec{
         void setMat (string m){
             mat = m;
         }
-        void printInfo (){
+        void printInfo (){ //Una funcion que saca toda la informacion junta
             cout << nom << ", " << age << ", " << ofi << ", matricula " << mat << ", " << ave << ", semestre " << sem;
-            hog.getInfo();
+            hog.getInfo(); //La funcion de residencia la aplicamos aqui
         }
 };
 #endif // ESTUDIANTES_H
