0832aef094754996bff8c141a63d0961620e11d7
marcotvaleriot <64273582+marcotvaleriot@users.noreply.github.com>
2020-04-29 23:13:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
+#include <string>
 #include <iostream>
+#include <sstream>
+
+#include "personatec.h" //traigo la clase de PersonaTec
+#include "estudiantes.h //traigo la clase de Estudiantes
+#include "profesores.h" //traigo la clase de Profesores
+#include "Residencia.h" //traigo la clase de Residencia
 
 using namespace std;
 
-#include "estudiantes.h
-#include "profesores.h"
-#include "mate.h"
 
 int main()
 {
-    Estudiantes e1(18, "Marco", "A01706518", "ICI", 2);
-    Profesores p1(30, "Juan Soto", "Catedra", "Negocios", 30000);
+    Residencia r1("San Felipe", "Apartamento", 4); //Creo una residencia
+    Residencia r2("San Joaquin", "Casa", 32); //Creo otra residencia
 
+    Estudiantes e1(18, "Marco", "A01706518", "ICI", 2, r1); //Creo un estudiante
+    Profesores p1(30, "Juan Soto", "Catedra", "Negocios", 30000, r2); //Creo un profesor
 
-    cout << e1.getAge() << ", " << e1.getOfi() << ", " << e1.getNom() << ", " << e1.getMat() << ", " << e1.getAve() << ", " << e1.getSem() << endl;
+    e1.printInfo(); //Imprimo la info del estudiante
+    p1.printInfo(); //Imprimo la info del profesor
 
+    e1.setAge(19); //Le cambio la edad al estudiante
+    e1.setSem(3); //Le cambio el semestre en que va
+    e1.printInfo(); //Veo la nueva info
 
 
 
