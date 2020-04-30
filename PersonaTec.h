f758d8763856416e332138d96abff089fd1d68bf
marcotvaleriot <64273582+marcotvaleriot@users.noreply.github.com>
2020-04-29 22:58:11 -0500

    Update PersonaTec.h

--- PersonaTec.h
+++ PersonaTec.h
@@ -1,23 +1,26 @@
 #ifndef PERSONATEC_H_
 #define PERSONATEC_H_
 
-#include <iostream>
 #include <string>
+#include <iostream>
 #include <sstream>
-
+#include "Residencia.h"
 
 using namespace std;
 
+
+
 class PersonaTec{
-    private://Atributos
+    public://Metodos
         string ofi;
         int age;
         string nom;
-    public://Metodos
-        PersonaTec (int edad, string oficio, string nombre){//Constructor
+        Residencia hog;
+        PersonaTec (int edad, string oficio, string nombre, Residencia hogar){//Constructor
             age = edad;
             ofi = oficio;
             nom = nombre;
+            hog = hogar;
         }
         int getAge(){
             return age;
@@ -28,8 +31,18 @@ class PersonaTec{
         string getNom(){
             return nom;
         }
-        setOfi(string o){
+        void setOfi(string o){
             ofi = o;
         }
-
+        void setAge(int a){
+            age = a;
+        }
+        void setNom(string n){
+            nom = n;
+        }
+        void printInfo (){
+            cout << nom << ", " << age << ", " << ofi << ", ";
+            hog.getInfo();
+        }
 };
+#endif // PERSONATEC_H_
